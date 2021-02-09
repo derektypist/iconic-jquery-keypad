530a6e897c80d2a3c5162244bc312fbdc3a93598
Derek Dhammaloka <derek_dhammaloka@yahoo.com>
2021-02-09 15:59:53 +0000

    Added functions to reset and display activities at script.js

--- js/script.js
+++ js/script.js
@@ -1,3 +1,22 @@
 $(document).ready(function() {
-    
+
+    // Set Up Global Variables
+    let $display = $("#display");
+    let iterations = 0;
+    const DISPLAY_BACKGROUND_COLORS = ["seagreen","blue","purple","orangered","olive","brown","deeppink","cornflowerblue"];
+    const MESSAGE = "Select Activity";
+
+    resetActivity();
+
+    // Function to Reset Activity
+    function resetActivity() {
+        displayActivity(MESSAGE);
+    }
+
+    // Function to Display Activity
+    function displayActivity(name) {
+        $("#display").html(`<p>${name}</p>`);
+    }
+
+
 });
